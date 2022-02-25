59a0ef58415859a4ee4076b8f9a210625698cbf0
godfrey-asiimwe <agtumusiime@gmail.com>
2022-02-25 16:28:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import Auth, { useAuthActions } from 'use-eazy-auth'
+import { AuthRoute, GuestRoute } from 'use-eazy-auth/routes'
+import { ConfigureRj } from 'react-rocketjump'
+import { map } from 'rxjs/operators'
+import { ajax } from 'rxjs/ajax'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login'
+import AddressBook from './pages/AddressBook'
 
-function App() {
+
+const login = (credentials = {}) =>
+  ajax({
+    url: '/token/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: credentials,
+  }).pipe(
+    map(({ response }) => ({
+      accessToken: response.access,
+      refreshToken: response.refresh,
+    }))
+  )
+
+
+const me = (token) =>
+  ajax.getJSON('/me/', {
+    Authorization: `Bearer ${token}`,
+  })
+
+const refresh = (refreshToken) =>
+  ajax({
+    url: '/token/refresh/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: { refresh: refreshToken },
+  }).pipe(
+    map(({ response }) => ({
+      refreshToken,
+      accessToken: response.access,
+    }))
+  )
+
+function ConfigureAuth({ children }) {
+  const { callAuthApiObservable } = useAuthActions()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ConfigureRj effectCaller={callAuthApiObservable}>
+      {children}
+    </ConfigureRj>
+  )
 }
 
-export default App;
+export default function App() {
+  return (
+    <Auth loginCall={login} meCall={me} refreshTokenCall={refresh}>
+      <ConfigureAuth>
+        <Router>
+          <Switch>
+            <GuestRoute path="/login" redirectTo='/'>
+              <Login />
+            </GuestRoute>
+            <AuthRoute path="/" exact redirectTo='/login'>
+              <AddressBook />
+            </AuthRoute>
+          </Switch>
+        </Router>
+      </ConfigureAuth>
+    </Auth>
+  )
+}
